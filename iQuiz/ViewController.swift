04a678d0f78582443293f47a4efa3bf39d703509
Vishank Rughwani <vishankr@uw.edu>
2021-05-06 02:01:12 -0700

    added cells

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SubjectsSource: NSObject, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return subject.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BasicStyle", for: indexPath)
+        cell.textLabel?.text = subject[indexPath.row]
+        return cell
+    }
+    
+    let subject = ["Mathematics", "Marvel Super Heroes", "Science"]
+}
 
+class ViewController: UIViewController{
+    
+    let data = SubjectsSource()
+
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = data
     }
 
 
